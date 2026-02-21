5847f91217839ef76aa727da1c873a6f3854139e
Leon <Leonhuber2010@gmail.com>
2026-02-21 22:31:57 +0100

    add dashboard screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import {Home} from './home/home';
+import {Class} from './class/class';
 import {Grades} from './grades/grades';
 import {Calendar} from './calendar/calendar';
 import {Timetable} from './timetable/timetable';
+import {Home} from './home/home';
 
 export const routes: Routes = [
   {path: "", component: Home},
+  {path: "class", component: Class},
   {path: "grades", component: Grades},
   {path: "calendar", component: Calendar},
   {path: "timetable", component: Timetable},
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,87 +1,41 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {ClassItem} from '../components/class-item/class-item';
+import {Component, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {Student} from '../components/student/student';
-import {Router} from '@angular/router';
+import {TimetableItem} from '../components/timetable-item/timetable-item';
 
 @Component({
   selector: 'app-home',
   imports: [
-    ClassItem,
-    NgIf,
-    FormsModule,
     NgForOf,
-    Student
+    TimetableItem,
+    NgIf
   ],
-  standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-
 export class Home implements OnInit {
-  constructor(private route: Router) { }
-  showAddClass = false
-  newClass: string = ""
-  newSubject: string = ""
-  showAddSubject = false
-  classes: any[] = []
-  currentClass: string = ""
-  students: any = []
-  currentSubject: string = ""
-  newStudent: string = ""
-  showAddStudent = false
+  timetable: any[] = []
 
 
   ngOnInit() {
-    const stored = localStorage.getItem('class');
-    const parsed = stored ? JSON.parse(stored) : [];
-    this.classes = parsed;
+    this.loadTimetable()
   }
 
-  async addClass() {
-    if (!this.newClass.trim()) return;
+  loadTimetable() {
+    const days = [
+      "sunday",
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday"
+    ]
 
-    const stored = localStorage.getItem('class');
-    const parsed = stored ? JSON.parse(stored) : [];
-    const newList = [{ class: this.newClass.trim() }, ...parsed];
-    localStorage.setItem("class", JSON.stringify(newList));
-    this.classes = newList;
-    this.newClass = "";
-    this.showAddClass = false;
-  }
+    const date = new Date()
+    const today = days[date.getDay()]
 
-  async addSubject() {
-    if (!this.newSubject.trim()) return;
-    const stored = localStorage.getItem(`${this.currentClass}/subjects`);
-    const parsed = stored ? JSON.parse(stored) : [];
-    const newList = [{ subject: this.newSubject.trim() }, ...parsed];
-    localStorage.setItem(`${this.currentClass}/subjects`, JSON.stringify(newList));
-    this.newSubject = "";
-    this.showAddSubject = false;
-    window.location.reload();
-  }
+    const stored = localStorage.getItem(`${today}`);
+    this.timetable = stored ? JSON.parse(stored) : [];
 
-  loadStudents(subject: string, className: string) {
-    this.currentSubject = subject;
-    const stored = localStorage.getItem(`${className}/${subject}/students`);
-    this.students = stored ? JSON.parse(stored) : [];
-    console.log(this.students)
-  }
-
-  addStudent(){
-    const stored = localStorage.getItem(`${this.currentClass}/${this.currentSubject}/students`);
-    const parsed = stored ? JSON.parse(stored) : [];
-    const newList = [{student: this.newStudent}, ...parsed]
-    localStorage.setItem(`${this.currentClass}/${this.currentSubject}/students`, JSON.stringify(newList));
-    this.students = newList;
-    this.newStudent = "";
-    this.showAddStudent = false;
-  }
-
-  goToGrades(student: string) {
-    this.route.navigate(['/grades'], {
-      queryParams: {student: student, class: this.currentClass, subject: this.currentSubject}
-    });
   }
 }
